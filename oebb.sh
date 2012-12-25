157357aa75b63bccf19ca752a79dbac40fd82159
Iulian Dogariu <code@iuliandogariu.com>
2012-12-25 20:39:14 +0200

    perl build now seems to work

--- oebb.sh
+++ oebb.sh
@@ -362,8 +362,7 @@ BB_GENERATE_MIRROR_TARBALLS = "0"
 # Go through the Firewall
 #HTTP_PROXY        = "http://${PROXYHOST}:${PROXYPORT}/"
 
-# Perl build fails under OE, so let's just use the native version.
-ASSUME_PROVIDED += "perl-native libxml-parser-perl-native openssl-native sgmlspl-native"
+ASSUME_PROVIDED += "libxml-parser-perl-native openssl-native sgmlspl-native"
 
 _EOF
 fi
